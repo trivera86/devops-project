9a6a29a36b1474d46f1e129afb5cdd25e0d005e1
Tyler Rivera <riverat2@email.chop.edu>
2017-08-03 16:05:17 -0400

    Log stream typo

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_group" "nginx" {
     name = "nginx"
 }
 
-resource "aws_cloudwatch_log_strean" "nginx" {
+resource "aws_cloudwatch_log_stream" "nginx" {
     name = "NginxLogStream"
     log_group_name = "${aws_cloudwatch_log_group.nginx.name}"
 }
